080988feb87f53d852ec0589dbdf504df4bb9b0a
François Colas <francoiscolas@gmail.com>
2019-10-31 23:35:33 +0100

    update Gulp and electron-builder

--- Gulpfile.js
+++ Gulpfile.js
@@ -6,53 +6,51 @@ var autoprefixer = require('gulp-autoprefixer');
 var include      = require('gulp-include');
 var exec         = require('child_process').exec;
 
-gulp.task('default', ['build']);
+gulp.task('css', () => {
+  return gulp.src('./src/scss/*.scss')
+    .pipe(sass({
+      outputStyle: 'compressed'
+    }))
+    .pipe(autoprefixer())
+    .pipe(gulp.dest('./app/assets/css'));
+});
 
-gulp.task('run', ['assets'], callback => {
+gulp.task('js', () => {
+  return gulp.src('./src/js/*.js')
+    .pipe(include())
+    .pipe(gulp.dest('./app/assets/js'));
+});
+
+gulp.task('assets', gulp.parallel('css', 'js'));
+
+gulp.task('watch', gulp.series('assets', () => {
+  gulp.watch('./src/scss/**/*.scss', ['css']);
+  gulp.watch('./src/js/**/*.js', ['js']);
+}));
+
+gulp.task('app', gulp.series('assets', callback => {
   exec('electron app', function (error, stdout, stderr) {
     console.log(stdout);
     console.log(stderr);
     callback(error);
   });
-});
+}));
 
-gulp.task('pack', ['assets'], callback => {
-  exec('build --dir', function (error, stdout, stderr) {
+gulp.task('prepare', gulp.series('assets', callback => {
+  exec('electron-builder build --dir', function (error, stdout, stderr) {
     console.log(stdout);
     console.log(stderr);
     callback(error);
   });
-});
+}));
 
-gulp.task('dist', ['assets'], callback => {
-  exec('build', function (error, stdout, stderr) {
+gulp.task('build', gulp.series('assets', callback => {
+  exec('electron-builder build', function (error, stdout, stderr) {
     console.log(stdout);
     console.log(stderr);
     callback(error);
   });
-});
+}));
 
-gulp.task('assets', ['css', 'js']);
-
-gulp.task('watch', ['assets'], () => {
-  gulp.watch('./src/scss/**/*.scss', ['css']);
-  gulp.watch('./src/js/**/*.js', ['js']);
-});
-
-gulp.task('css', () => {
-  gulp.src('./src/scss/*.scss')
-    .pipe(sass({
-      outputStyle: 'compressed'
-    }))
-    .pipe(autoprefixer({
-      browsers: ['last 2 versions', 'ie >= 9', 'and_chr >= 2.3']
-    }))
-    .pipe(gulp.dest('./app/assets/css'));
-});
-
-gulp.task('js', () => {
-  gulp.src('./src/js/*.js')
-    .pipe(include())
-    .pipe(gulp.dest('./app/assets/js'));
-});
+gulp.task('default', gulp.series('build'));
 
