francoiscolas/gdp

commit 17d742be4699268229a367df96f4c88372165e96

ui is now usable on mobile (but not optimized)

src/renderer/index.scss
@@ -1,9 +1,11 @@
 @charset "utf-8";
 @import 'bulma/bulma.sass';
 
-html, body {
-  overflow: hidden;
-  height: 100%;
+@include tablet {
+  html, body {
+    overflow: hidden;
+    height: 100%;
+  }
 }
 
 @import './scss/main';

src/renderer/scss/main.scss
@@ -4,12 +4,11 @@ body#app.main {
 
   #sources-panel {
     background: $light;
-    #sources-search {
-      padding-bottom: $block-spacing / 2;
-    }
+    min-height: 100vh;
     #sources-list {
-      height: calc(100% - 52px);
+      height: calc(100% - 40px);
       overflow-y: auto;
+      padding-top: $block-spacing / 2;
       .item {
         display: flex;
         align-items: center;
@@ -26,6 +25,11 @@ body#app.main {
           white-space: nowrap;
         }
       }
+    }
+  }
+  @include mobile {
+    #sources-panel {
+      min-height: auto;
     }
   }
 
@@ -53,6 +57,7 @@ body#app.main {
 
   .source-panel {
     @extend .is-relative;
+
     height: 100%;
     .source-actions {
       .level-left {

src/renderer/views/mainview.js
@@ -61,7 +61,7 @@ let MainView = Backbone.View.extend({
   },
 
   template: _.template(`
-    <div class="column is-one-quarter hero is-fullheight" id="sources-panel">
+    <div class="column is-one-quarter hero" id="sources-panel">
       <div id="sources-search">
         <div class="control has-icons-left">
           <input class="input" type="text" placeholder="Recherche (ALT+R) ...">
@@ -70,7 +70,7 @@ let MainView = Backbone.View.extend({
           </span>
         </div>
       </div>
-      <div id="sources-list"></div>
+      <div class="is-hidden-mobile" id="sources-list"></div>
     </div>
     <div class="column" id="test"></div>
     <div class="column" id="display"></div>

src/renderer/views/sourceview.js
@@ -12,7 +12,7 @@ let SourceView = Backbone.View.extend({
     <div class="panel source-panel">
       <div class="panel-heading"><%= this.viewName %></div>
       <div class="panel-block is-block source-actions">
-        <div class="level">
+        <div class="level is-mobile">
           <div class="level-left">
             <div class="subtitle"></div>
           </div>

commit c2d9ec20d1ddddd6f24555730c46d2659d724256

fix responsive bug introduced by 17d742be

src/renderer/index.scss
@@ -1,11 +1,9 @@
 @charset "utf-8";
 @import 'bulma/bulma.sass';
 
-@include tablet {
-  html, body {
-    overflow: hidden;
-    height: 100%;
-  }
+html, body {
+  overflow: hidden;
+  height: 100%;
 }
 
 @import './scss/main';

src/renderer/router.js
@@ -1,6 +1,7 @@
 'use strict';
 
 let _        = require('lodash');
+let $        = require('jquery');
 let Backbone = require('backbone');
 
 class Router extends Backbone.Router {
@@ -11,6 +12,11 @@ class Router extends Backbone.Router {
     this.route('settings', 'settings');
     this.route('associate', 'associate');
     this.route('about', 'about');
+  }
+
+  execute(callback, args, name) {
+    super.execute(callback, args, name);
+    $('html').addClass('ep-' + name);
   }
 
   index() {

src/renderer/scss/about.scss
@@ -1,4 +1,4 @@
-body#app.about {
+html.ep-about body {
   display: flex;
   justify-content: center;
   align-items: center;

src/renderer/scss/associate.scss
@@ -1,4 +1,4 @@
-body#app.associate {
+html.ep-associate body {
   display: flex;
   justify-content: center;
   align-items: center;

src/renderer/scss/main.scss
@@ -1,10 +1,16 @@
-body#app.main {
+@include mobile {
+  html.ep-index, html.ep-index body {
+    overflow: auto;
+    height: auto;
+  }
+}
+html.ep-index body {
   @extend .columns;
   @extend .is-marginless;
 
   #sources-panel {
     background: $light;
-    min-height: 100vh;
+    min-height: auto;
     #sources-list {
       height: calc(100% - 40px);
       overflow-y: auto;
@@ -25,11 +31,6 @@ body#app.main {
           white-space: nowrap;
         }
       }
-    }
-  }
-  @include mobile {
-    #sources-panel {
-      min-height: auto;
     }
   }
 

src/renderer/scss/screen.scss
@@ -1,4 +1,4 @@
-body#app.screen {
+html.ep-screen body {
   background-color: #000000;
   background-position: center center;
   background-repeat: no-repeat;

src/renderer/scss/settings.scss
@@ -1,4 +1,4 @@
-body#app.settings {
+html.ep-settings body {
   display: flex;
   justify-content: center;
   align-items: center;

src/renderer/views/aboutview.js
@@ -21,10 +21,6 @@ let AboutView = Backbone.View.extend({
     </div>
   `),
 
-  initialize: function () {
-    this.$el.addClass('about');
-  },
-
   render: function () {
     this.$el.html(this.template({App: App}));
   },

src/renderer/views/associateview.js
@@ -27,10 +27,6 @@ let AssociateView = Backbone.View.extend({
     </div>
   `),
 
-  initialize: function () {
-    this.$el.addClass('associate');
-  },
-
   render: function () {
     this.$el.html(this.template({
       ip: _.reduce(OS.networkInterfaces(), function (memo, addresses) {

src/renderer/views/mainview.js
@@ -77,7 +77,6 @@ let MainView = Backbone.View.extend({
   `),
 
   initialize: function () {
-    this.$el.addClass('main')
     this.$el.html(this.template())
 
     this.display = new Display()

src/renderer/views/screenview.js
@@ -26,8 +26,6 @@ var ScreenView = Backbone.View.extend({
 
     this.listenTo(this.settings, 'change', this.render);
     this.listenTo(this.sources, 'add', this.render);
-
-    this.$el.addClass('screen');
   },
 
   render: function () {

src/renderer/views/settingsview.js
@@ -63,10 +63,6 @@ let SettingsView = Backbone.View.extend({
     'click #cancel-btn'     : 'close',
   },
 
-  initialize: function () {
-    this.$el.addClass('settings');
-  },
-
   $bgColor: function () {
     return this.$('#bg-color-input');
   },

src/renderer/views/sourceview.js
@@ -44,23 +44,18 @@ let SourceView = Backbone.View.extend({
     this.source = null;
     this.currentPage = 1;
     this.pdfpromise = null;
-    this.$subtitle = null;
-    this.$pages = null;
-    this.$content = null;
-    this.canvas = null;
+
+    this.$el.html(this.template());
+    this.$subtitle = this.$('.subtitle');
+    this.$pages = this.$('button:disabled');
+    this.$content = this.$('.source-content');
+    this.canvas = this.$('canvas').get(0);
+
     this.listenTo(this.display, 'change:bgColor', this.render);
     this.listenTo(this.display, 'change:bgImage', this.render);
   },
 
   render: function () {
-    if (this.$el.is(':empty')) {
-      this.$el.html(this.template());
-      this.$subtitle = this.$('.subtitle');
-      this.$pages = this.$('button:disabled');
-      this.$content = this.$('.source-content');
-      this.canvas = this.$('canvas').get(0);
-    }
-
     this.$subtitle.text(this.source ? this.source.get('name') : '');
     this.$pages.text('-/-');
     this.$content.css({